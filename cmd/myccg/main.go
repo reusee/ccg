6e25463bf3c61818c3c4f6930e505285ac5e3b19
reus <reusee@gmail.com>
2015-03-08 00:01:29 +0800

    myccg: refactor

--- cmd/myccg/main.go
+++ cmd/myccg/main.go
@@ -29,52 +29,61 @@ func main() {
 		return
 	}
 
-	getArg := func(index int, usage string) string {
-		if len(args) < index+1 {
-			pt("usage: %s %s\n", os.Args[0], usage)
-			os.Exit(-1)
-		}
-		return args[index]
+	type CmdSpec struct {
+		Usage   string
+		Params  []string
+		Renames []string
 	}
 
-	command := args[0]
-	switch command {
-	case "sorter":
-		usage := "sorter [type] [sorter type]"
-		t := getArg(1, usage)
-		sorterType := getArg(2, usage)
-		var writer io.Writer
-		if *outputFile != "" {
-			var err error
-			writer, err = os.Create(*outputFile)
-			if err != nil {
-				log.Fatalf("myccg: create file %s error: %v", *outputFile, err)
-			}
-			defer writer.(*os.File).Close()
-			if *packageName == "" {
-				main := "main"
-				packageName = &main
-			}
-		} else {
-			writer = new(bytes.Buffer)
+	specs := map[string]CmdSpec{
+		"sorter": {
+			"[element type] [sorter name]",
+			[]string{"T"},
+			[]string{"Sorter"},
+		},
+	}
+
+	spec, ok := specs[args[0]]
+	if !ok {
+		log.Fatalf("unknown subcommand %s", args[0])
+	}
+	if len(args[1:]) != len(spec.Params)+len(spec.Renames) {
+		log.Fatalf("usage: %s %s %s", os.Args[0], args[0], spec.Usage)
+	}
+	params := map[string]string{}
+	for i, param := range spec.Params {
+		params[param] = args[1+i]
+	}
+	renames := map[string]string{}
+	start := len(spec.Params)
+	for i, orig := range spec.Renames {
+		renames[orig] = args[1+start+i]
+	}
+
+	var writer io.Writer
+	if *outputFile != "" {
+		var err error
+		writer, err = os.Create(*outputFile)
+		if err != nil {
+			log.Fatalf("myccg: create file %s error: %v", *outputFile, err)
 		}
-		ccg.Copy(ccg.Config{
-			From: "github.com/reusee/ccg/sorter",
-			Params: map[string]string{
-				"T": t,
-			},
-			Renames: map[string]string{
-				"Sorter": sorterType,
-			},
-			Writer:  writer,
-			Package: *packageName,
-		})
-		if *outputFile != "" {
-		} else {
-			pt("%s\n", writer.(*bytes.Buffer).Bytes())
+		defer writer.(*os.File).Close()
+		if *packageName == "" {
+			main := "main"
+			packageName = &main
 		}
-	default:
-		pt("unknown command: %s\n", command)
-		return
+	} else {
+		writer = new(bytes.Buffer)
+	}
+
+	ccg.Copy(ccg.Config{
+		From:    "github.com/reusee/ccg/" + args[0],
+		Params:  params,
+		Renames: renames,
+		Writer:  writer,
+		Package: *packageName,
+	})
+	if *outputFile == "" {
+		pt("%s\n", writer.(*bytes.Buffer).Bytes())
 	}
 }
