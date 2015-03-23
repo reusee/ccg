4cc568c1a48bd0c55f312e8714d7811498c5df6f
reus <reusee@gmail.com>
2015-03-23 21:53:11 +0800

    remove dead code

--- utils.go
+++ utils.go
@@ -6,10 +6,6 @@ import (
 	"golang.org/x/tools/go/types"
 )
 
-type ObjectSet map[types.Object]struct{}
-
-type AstSpecs []ast.Spec
-
 type AstDecls []ast.Decl
 
 func (s AstDecls) Filter(filter func(ast.Decl) bool) (ret AstDecls) {
@@ -21,6 +17,8 @@ func (s AstDecls) Filter(filter func(ast.Decl) bool) (ret AstDecls) {
 	return
 }
 
+type AstSpecs []ast.Spec
+
 func (s AstSpecs) Filter(filter func(ast.Spec) bool) (ret AstSpecs) {
 	for _, elem := range s {
 		if filter(elem) {
@@ -30,6 +28,8 @@ func (s AstSpecs) Filter(filter func(ast.Spec) bool) (ret AstSpecs) {
 	return
 }
 
+type ObjectSet map[types.Object]struct{}
+
 func NewObjectSet() ObjectSet {
 	return ObjectSet(make(map[types.Object]struct{}))
 }
@@ -38,24 +38,7 @@ func (s ObjectSet) Add(t types.Object) {
 	s[t] = struct{}{}
 }
 
-func (s ObjectSet) Del(t types.Object) {
-	delete(s, t)
-}
-
 func (s ObjectSet) In(t types.Object) (ok bool) {
 	_, ok = s[t]
-	return
-}
-
-func (s ObjectSet) Each(fn func(types.Object)) {
-	for e := range s {
-		fn(e)
-	}
-}
-
-func (s ObjectSet) Slice() (ret []types.Object) {
-	for e := range s {
-		ret = append(ret, e)
-	}
 	return
 }
