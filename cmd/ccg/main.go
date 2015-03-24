eddbc3804a77e6ce20b5085d6bebef567d6599b5
reus <reusee@gmail.com>
2015-03-25 00:33:49 +0800

    ccg: various fixes

--- cmd/ccg/main.go
+++ cmd/ccg/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"strings"
 
+	"go/ast"
+	"go/parser"
+	"go/token"
+
+	"github.com/jessevdk/go-flags"
 	"github.com/reusee/ccg"
 )
 
@@ -14,35 +19,41 @@ var (
 	pt = fmt.Printf
 )
 
+var opts struct {
+	From    string `short:"f" description:"template package import path"`
+	Params  string `short:"t" description:"parameters"`
+	Renames string `short:"r" description:"renames"`
+	Package string `short:"p" description:"output package name"`
+	Output  string `short:"o" description:"output file path"`
+	Uses    string `short:"u" description:"names to be used only"`
+}
+
 func main() {
-	fromPkg := flag.String("from", "", "package to read from")
-	typeParamsStr := flag.String("params", "", "comma-separated param=arg pairs of parameterize spec")
-	renamesStr := flag.String("renames", "", "comma-separated old=new pairs of rename spec")
-	packageName := flag.String("package", "", "package name of output file")
-	flag.Parse()
-
-	// check source package
-	if len(*fromPkg) == 0 {
-		log.Fatalf("no package specified")
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// check type parameters
-	if len(*typeParamsStr) == 0 {
-		log.Fatalf("no type parameter specified")
+	if len(opts.From) == 0 {
+		log.Fatal("no template package specified")
 	}
-	typeParams := map[string]string{}
-	for _, pairStr := range strings.Split(*typeParamsStr, ",") {
-		pair := strings.SplitN(pairStr, "=", 2)
-		if len(pair) != 2 {
-			log.Fatalf("invalid parameterize spec: %s", pairStr)
+
+	// params
+	params := map[string]string{}
+	if len(opts.Params) > 0 {
+		for _, pairStr := range strings.Split(opts.Params, ",") {
+			pair := strings.SplitN(pairStr, "=", 2)
+			if len(pair) != 2 {
+				log.Fatalf("invalid parameterize spec: %s", pairStr)
+			}
+			params[pair[0]] = pair[1]
 		}
-		typeParams[pair[0]] = pair[1]
 	}
 
-	// check renames
+	// renames
 	renames := map[string]string{}
-	if len(*renamesStr) > 0 {
-		for _, pairStr := range strings.Split(*renamesStr, ",") {
+	if len(opts.Renames) > 0 {
+		for _, pairStr := range strings.Split(opts.Renames, ",") {
 			pair := strings.SplitN(pairStr, "=", 2)
 			if len(pair) != 2 {
 				log.Fatalf("invalid rename spec: %s", pairStr)
@@ -52,12 +63,47 @@ func main() {
 	}
 
 	buf := new(bytes.Buffer)
-	ccg.Copy(ccg.Config{
-		From:    *fromPkg,
-		Params:  typeParams,
+	var decls []ast.Decl
+	fileSet := new(token.FileSet)
+	if opts.Output != "" {
+		content, err := ioutil.ReadFile(opts.Output)
+		if err == nil {
+			astFile, err := parser.ParseFile(fileSet, opts.Output, content, 0)
+			if err == nil {
+				decls = astFile.Decls
+			}
+		}
+		if opts.Package == "" {
+			opts.Package = "main"
+		}
+	}
+
+	var usesNames []string
+	if len(opts.Uses) > 0 {
+		for _, name := range strings.Split(opts.Uses, ",") {
+			usesNames = append(usesNames, name)
+		}
+	}
+
+	err = ccg.Copy(ccg.Config{
+		From:    opts.From,
+		Params:  params,
 		Renames: renames,
 		Writer:  buf,
-		Package: *packageName,
+		Package: opts.Package,
+		Decls:   decls,
+		FileSet: fileSet,
+		Uses:    usesNames,
 	})
-	pt("%s\n", buf.Bytes())
+	if err != nil {
+		log.Fatalf("ccg: copy error %v", err)
+	}
+	if opts.Output == "" {
+		pt("%s\n", buf.Bytes())
+	} else {
+		err = ioutil.WriteFile(opts.Output, buf.Bytes(), 0644)
+		if err != nil {
+			log.Fatalf("ccg: write file error %v", err)
+		}
+	}
 }
